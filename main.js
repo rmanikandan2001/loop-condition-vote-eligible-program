1d73b657f3066d6d8ff133cdbc036bf624f502a6
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-15 23:24:53 -0700

    add

---
+++ main.js
@@ -0,0 +1,31 @@
+
+ // voter id 
+ 
+ 
+ let=prompt("enter a number");
+ 
+ if(let>18){
+	 
+	console.log("voter is eligible");
+ }
+else{
+	
+
+		console.log("voter is not eligible");
+}
+
+//leap year
+
+// let=prompt("enter a number");
+
+// if(year%4){
+	 
+	// console.log("leap is eligible");
+ // }
+// else{
+	
+
+		// console.log("leap is not eligible");
+// }
+
+
